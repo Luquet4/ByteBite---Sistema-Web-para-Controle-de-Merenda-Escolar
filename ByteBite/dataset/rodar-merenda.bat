c7aa15e22a8819c28472a8d10f2670073ba4d771
Lucas Gabriel V. M. Bezerra <110059687+Luquet4@users.noreply.github.com>
2024-01-09 11:40:37 -0300

    Delete Dataset

--- ByteBite/dataset/rodar-merenda.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd C:\Users\Lucas Gabriel\Desktop\VirtualFlask\FLASK\
-call venv\Scripts\activate
-cd C:\Users\Lucas Gabriel\Desktop\VirtualFlask\FLASK\Python-Flask
-start "" http://localhost:5000
-flask --app app.py --debug run
